19f157cd1e747d1e4bac3d0e64ce52fcaf39cfc7
Jakub Kohoutek <kohoutek-jakub@seznam.cz>
2022-11-27 02:50:50 +0100

    Implemented abstract class BasePresenter.php

--- app/Presenters/BasePresenter.php
+++ app/Presenters/BasePresenter.php
@@ -4,5 +4,21 @@ namespace App\Presenters;
 
 abstract class BasePresenter extends \Nette\Application\UI\Presenter
 {
+    public function setup($access = null)
+    {
+        // Is user logged in?
+        if (!$this->getUser()->isLoggedIn()) {
+            $this->redirect('Sign:in');
+        }
 
+        // Is user admin?
+        if (isset($access)) {
+            if (!$this->getUser()->isInRole($access)) {
+                $this->flashMessage('Nemáte dostatečná oprávnění.', 'danger');
+                $this->redirect(':Homepage');
+            }
+        }
+
+        $this->template->user = $this->getUser()->getIdentity();
+    }
 }
--- app/Presenters/DashboardPresenter.php
+++ app/Presenters/DashboardPresenter.php
@@ -4,35 +4,13 @@ declare(strict_types=1);
 namespace App\Presenters;
 use Nette;
 
-final class DashboardPresenter extends Nette\Application\UI\Presenter
+final class DashboardPresenter extends BasePresenter
 {
     public function renderDefault(): void
     {
         $this->setup();
     }
 
-    private function setup($access = null)
-    {
-        // Is user logged in?
-        if (!$this->getUser()->isLoggedIn()) {
-            $this->redirect('Sign:in');
-        }
 
-        // Is user admin?
-        if (isset($access)) {
-            if (!$this->getUser()->isInRole($access)) {
-                $this->flashMessage('Nemáte dostatečná oprávnění.', 'danger');
-                $this->redirect(':Homepage');
-            }
-        }
-
-        // Set template variables
-        /*
-        $this->template->pageName = '';
-        $this->template->pageNameCs = 'Přehled';
-        $this->template->pageModules = [];
-        */
-        $this->template->user = $this->getUser()->getIdentity();
-    }
 
 }
