b5e84668eb59ce423043657c5d523789df2e6048
Jakub Kohoutek <62189043+JustAKuba@users.noreply.github.com>
2023-04-17 02:21:32 +0200

    Updated Source Codes

--- database.sql
+++ database.sql
@@ -1,54 +1,105 @@
+create table help_article
+(
+    id           int auto_increment
+        primary key,
+    name         varchar(255) not null,
+    content      text null,
+    author       int          not null,
+    created_at   datetime default current_timestamp() null,
+    date_deleted datetime null,
+    deleted_by   int null,
+    chapter      int null,
+    tech_name    varchar(255) null,
+    is_deleted   tinyint(1) default 0 null,
+    constraint help_article_id_uindex
+        unique (id)
+);
 
-SET NAMES utf8;
-SET foreign_key_checks = 0;
+create table help_chapter
+(
+    id           int auto_increment,
+    name         varchar(255) not null,
+    created_at   datetime default current_timestamp() null,
+    date_deleted datetime null,
+    deleted_by   int null,
+    tech_name    varchar(255) null,
+    is_deleted   tinyint(1) default 0 null,
+    constraint help_chapter_id_uindex
+        unique (id)
+);
 
-CREATE TABLE `comments` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `post_id` int(11) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `content` text NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `post_id` (`post_id`),
-  CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+create table station
+(
+    id           int auto_increment
+        primary key,
+    address      varchar(255) null,
+    size         int not null comment '' Max number of vehicles
+        '',
+    phone        varchar(255) null,
+    name         varchar(255) null,
+    created_at   datetime default current_timestamp() null,
+    is_active    tinyint(1) default 1 null,
+    date_deleted datetime null,
+    deleted_by   int null,
+    is_deleted   tinyint(1) default 0 null,
+    constraint station_id_uindex
+        unique (id)
+);
 
+create table ticket
+(
+    id               int auto_increment
+        primary key,
+    urgency          int          not null,
+    state            int          not null,
+    vehicle          int null comment '' Možná rework na grupu '',
+    location_address varchar(255) null,
+    location_gps     varchar(255) null,
+    summary          varchar(255) not null,
+    description      text         not null,
+    created_at       datetime default current_timestamp() null,
+    date_deleted     datetime null,
+    deleted_by       int null,
+    is_deleted       int      default 0 null,
+    constraint ticket_id_uindex
+        unique (id)
+);
 
-CREATE TABLE `posts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL,
-  `content` text NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+create table user
+(
+    id           int auto_increment comment '' Primary Key ''
+        primary key,
+    email        varchar(255) not null comment '' User Email '',
+    password     varchar(255) not null comment '' User Password '',
+    role         varchar(255) not null comment '' User Role '',
+    created_at   datetime default current_timestamp() null comment '' Create Time '',
+    name         varchar(255) null,
+    is_active    tinyint  default 1 null,
+    date_deleted datetime null,
+    deleted_by   int null,
+    is_deleted   tinyint(1) default 0 null,
+    constraint user_email_uindex
+        unique (email),
+    constraint user_id_uindex
+        unique (id)
+);
 
-
--- sample data
-
-INSERT INTO `comments` (`id`, `post_id`, `name`, `email`, `content`, `created_at`) VALUES
-(1,	1,	'Jakub',	NULL,	'Wort Dressed Sentinent Being water quite a moment and show thirty speck by the floor. brightness glowed at least, nearly dead and was obviously some Vegan Rhino\'s cutlet. It\'s unpleasantly like hitch hiking slang, as anything. - said Zaphod. - Y',	'2009-05-11 07:06:05'),
-(2,	2,	'Ondřej',	NULL,	'Enormous round and guidance system will jump haven\'t opened through the faintly irritated him - That\'s just to Cassette recorder, every to to thirty seconds of us. Arthur began to have Wow, - said to discover into off with pleased with ',	'2009-05-19 23:23:21'),
-(3,	2,	'Gabriel',	NULL,	'Ape-descendant Arthur Dent, and equally get a stone sundial pedestal housed The mice He looked up sharply. He threw Ford handed the Earth passed an answer. - You know, not even finished Permeated - He adjusted it. Arthur agreed with the time',	'2009-05-20 04:40:48'),
-(4,	2,	'Jakub',	NULL,	'Cracked bell, feet up. - Are you will finally managed to see very strong desire just happens. Yeah, I bother please, the not be, - Missiles? Don\'t talk about the common light Slurrp almost to come and the other bits consequences get there ',	'2009-05-20 05:14:31'),
-(5,	2,	'Daniel',	NULL,	'Emphasized because, as the white mice sniffed irritably decided that the ship that the sweaty dishevelled clothes he was for Arthur shivered with Deep Thought, - protested Ford. - said by your brain was almost, miles is each other. Fook ',	'2009-05-20 08:31:40'),
-(6,	2,	'Emily',	NULL,	'Desk. bubble, the wrong bit and the Earth years, maybe that anyone who could get the Sirius Cybernetics Corporation defines a moment, relax and so I\'ve heard rumors about in all intelligent that one pot shot out before a planet ',	'2009-05-20 09:52:00'),
-(7,	3,	'Olivia',	NULL,	'Silly antennae on the thirty seconds later he said. - Yes, - I\'m President always used to give it then? - Well? - Oh into the cold mud. It was clearly was built, and local affairs that\'s for a wicked grin, laugh did we knew much as the spectacle',	'2009-05-28 01:50:18'),
-(8,	3,	'Vojtěch',	NULL,	'Fact! bubble, the wrong bit and the Earth years, maybe that anyone who could get the Sirius Cybernetics Corporation defines a moment, relax and so I\'ve heard rumors about in all ',	'2009-05-28 10:06:31'),
-(9,	3,	'William',	NULL,	'Protruding from years, maybe even myself? slippers, ladder, moon, nightfall was at each other cajoleries and down Diurnal course. - A man frowned at his semi-cousin that through the faintly irritated him - That\'s just to ',	'2009-05-28 17:25:41'),
-(10,	3,	'Simon',	NULL,	'Minds big hello said Arthur. - I will finally realized that he said, - it was only fooling, - What is an interstellar distances in front partly More gunk music and it had nervously, I ',	'2009-05-28 23:25:25'),
-(11,	3,	'Amelia',	NULL,	'Ape-descendant Arthur Dent, and equally get a stone sundial pedestal housed The mice He looked up sharply. He threw Ford handed the Earth passed an answer. - You know, not even finished Permeated - He adjusted it. Arthur agreed with the time',	'2009-05-29 06:19:14'),
-(12,	4,	'Emily',	NULL,	'Violent noise leapt to thirty seconds later he said. - Yes, - I\'m President always used to give it then? - Well? - Oh into the cold mud. It was clearly was built, and local affairs that\'s for a wicked grin, laugh did we knew ',	'2009-06-08 17:07:21'),
-(13,	4,	'Jessica',	NULL,	'Air cushions ballooned out white mice sniffed irritably decided that the ship that the sweaty dishevelled clothes he was for ',	'2009-06-08 21:10:34'),
-(14,	4,	'Elias',	NULL,	'Demarcation may or the wrong bit and the Earth years, maybe that anyone who could get the Sirius Cybernetics Corporation defines a moment, relax and so I\'ve heard rumors about in all intelligent that one pot shot out before a planet ',	'2009-06-09 04:40:35'),
-(15,	5,	'Jessica',	NULL,	'Hence the slow heavy river Moth; wet of the time fresh whalemeat. At lunchtime? The Vogon guard dragged them brightness glowed at least, nearly dead and was obviously some Vegan Rhino\'s cutlet. It\'s unpleasantly like hitch hiking slang, as Tru',	'2009-06-19 01:56:47'),
-(16,	5,	'Joshua',	NULL,	'Optician almost to come and the other bits consequences get there now. The other illusory somewhere brushed backwards of how was was a sharp ringing tones. - he said Slartibartfast coughed politely. - moment in Stone. It saved a white',	'2009-06-19 03:44:05'),
-(17,	5,	'Lukas',	NULL,	'Desk. bubble, the wrong bit and the Earth years, maybe that anyone who could get the Sirius Cybernetics Corporation defines a moment, relax and so I\'ve heard rumors about in all intelligent that one pot shot out before a planet ',	'2009-06-19 07:16:40'),
-(18,	5,	'Grace',	NULL,	'Dent sat on him. - Yeah, OK, - Oh those doors. There must have something else. Come on, to help him, small really thrash it space that ',	'2009-06-19 07:28:33');
-
-INSERT INTO `posts` (`id`, `title`, `content`, `created_at`) VALUES
-(1,	'Thronged with making you doing',	'Out! looked like it. At an anachronism. The Dentrassis fine, moon, nightfall was at each other cajoleries and down there? - said Arthur turned himself up. - The that now six know the Universe, and it to know directly his seemed certain carbon-ba',	'2009-05-11 03:31:16'),
-(2,	'Jerked himself feet up',	'Refit, and found to come and the other bits consequences get there now. The other illusory somewhere brushed backwards of how was was a sharp ringing tones. - he said Slartibartfast coughed. Otherwise me. - He passed right between was b',	'2009-05-19 15:24:30'),
-(3,	'Danger',	'Usually had to one would I know, - Oh those doors. There must have something else. Come on, to help him, small really thrash it space that now six know the Universe, and it to know directly his seemed certain carbon-based life and.',	'2009-05-28 01:18:15'),
-(4,	'Tossed looked like it',	'Busy? - Just shut up, that spaceship and spewed up in emergencies as such, but... - yelled Ford, - said Arthur Dent with me? - said about to a rather into his neck. The President of the planet Bethselamin soft and said, very fast. Very good. For wha',	'2009-06-08 13:17:21'),
-(5,	'Eddie your eyes...',	'Airlock hatchway into your house down! Ford Prefect\'s were it for Magrathea, immediate sense in major intestine, in a solid small really thrash it space that now six know the Universe, and it to know directly. House Down Once you talked to',	'2009-06-18 23:55:45');
+create table vehicle
+(
+    id           int auto_increment
+        primary key,
+    callsign     varchar(255) not null,
+    model        varchar(255) null,
+    station      int null,
+    on_station   tinyint(1) default 1 null,
+    created_at   datetime default current_timestamp() null,
+    date_deleted datetime null,
+    deleted_by   int null,
+    is_deleted   tinyint(1) default 0 null,
+    constraint vehicle_callsign_uindex
+        unique (callsign),
+    constraint vehicle_id_uindex
+        unique (id)
+);
+-- Insert password php has admin
+INSERT INTO user(email, password, role, name) VALUES ('admin@admin.cz', '$2y$10$dBNtemQOdjHgIHL8JPZLk.rvbHF5vM.JVOHRnBb8UbjxcBq5yF0ja', 'Admin', 'Administrator');
